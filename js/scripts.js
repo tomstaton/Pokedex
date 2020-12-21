dad6b95cd27684dbf1938cb43408051179e92cc8
tomstaton <74940768+tomstaton@users.noreply.github.com>
2020-12-20 21:16:24 -0500

    add additional pokemon thru 65

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,379 @@
 pokemonList = [
-  //{name: 'pokemon', height: number, types: ['array of strings', '...']}
-  { name: 'Squirtle', height: 3, types: ['water']},
-  { name: 'Wartortle', height: 3, types: ['water']},
-  { name: 'Blastoise', height: 3, types: ['water']},
+  //{
+  //  name: 'pokemon',
+  //  height: number,
+  //  weight: number,
+  //  types: ['array of strings', '...']
+//}
+  { 
+    name: 'Squirtle',
+    height: .5,
+    weight: 9,
+    types: ['Water']
+  },
+  {
+    name: 'Wartortle',
+    height: 1,
+    weight: 22.5,
+    types: ['Water']
+  },
+  {
+    name: 'Blastoise',
+    height: 1.6,
+    weight: 85.5,
+    types: ['Water']
+  },
+  {
+    name: 'Charmander',
+    height: .6,
+    weight: 8.5,
+    types: ['Fire']
+  },
+  {
+    name: 'Charmeleon',
+    height: 1.1,
+    weight: 19,
+    types: ['Fire']
+  },
+  {
+    name: 'Charizard',
+    height: 1.7,
+    weight: 90.5,
+    types: ['Fire', 'Flying']
+  },
+  {
+    name: 'Bulbasaur',
+    height: .7,
+    weight: 6.9,
+    types: ['Grass', 'Poison']
+  },
+  {
+    name: 'Ivysaur',
+    height: 1,
+    weight: 13,
+    types: ['Grass', 'Poison']
+  },
+  {
+    name: 'Venasaur',
+    height: 2,
+    weight: 100,
+    types: ['Grass', 'Poison']
+  },
+  {
+    name: 'Caterpie',
+    height: .3,
+    weight: 2.9,
+    types: ['Bug'],
+  },
+  {
+    name: 'Metapod',
+    height: .7,
+    weight: 9.9,
+    types: ['Bug'],
+  },
+  {
+    name: 'Butterfree',
+    height: 1.1,
+    weight: 32,
+    types: ['Bug', 'Flying'],
+  },
+  {
+    name: 'Weedle',
+    height: .3,
+    weight: 3.2,
+    types: ['Bug', 'Poison'],
+  },
+  {
+    name: 'Kakuna',
+    height: .6,
+    weight: 10,
+    types: ['Bug', 'Poison'],
+  },
+  {
+    name: 'Beedrill',
+    height: 1,
+    weight: 29.5,
+    types: ['Bug', 'Poison'],
+  },
+  {
+    name: 'Pidgey',
+    height: .3,
+    weight: 1.8,
+    types: ['Flying', 'Normal'],
+  },  {
+    name: 'Pidgeotto',
+    height: 1.1,
+    weight: 30,
+    types: ['Flying', 'Normal'],
+  },  {
+    name: 'Pidgeot',
+    height: 1.5,
+    weight: 39.5,
+    types: ['Flying', 'Normal'],
+  },  {
+    name: 'Spearow',
+    height: .3,
+    weight: 2,
+    types: ['Flying', 'Normal'],
+  },  {
+    name: 'Fearow',
+    height: 1.2,
+    weight: 38,
+    types: ['Flying', 'Normal'],
+  },  {
+    name: 'Rattata',
+    height: .3,
+    weight: 3.5,
+    types: ['Normal'],
+  },  {
+    name: 'Raticate',
+    height: .7,
+    weight: 18.5,
+    types: ['Normal'],
+  },  {
+    name: 'Ekans',
+    height: 2,
+    weight: 6.9,
+    types: ['Poison'],
+  },
+  {
+    name: 'Arbok',
+    height: 3.5,
+    weight: 65,
+    types: ['Poison'],
+  },  {
+    name: 'Pikachu',
+    height: .4,
+    weight: 6,
+    types: ['Electric'],
+  },  {
+    name: 'Raichu',
+    height: .8,
+    weight: 30,
+    types: ['Electric'],
+  },  {
+    name: 'Sandshrew',
+    height: .6,
+    weight: 12,
+    types: ['Ground'],
+  },  {
+    name: 'Sandslash',
+    height: 1,
+    weight: 29.5,
+    types: ['Ground'],
+  },  {
+    name: 'Nidoran / "F" ',
+    height: .4,
+    weight: 7,
+    types: ['Poison'],
+  },  {
+    name: 'Nidorina',
+    height: .8,
+    weight: 20,
+    types: ['Poison'],
+  },
+  {
+    name: 'Nidoqueen',
+    height: 1.3,
+    weight: 60,
+    types: ['Ground', 'Poison'],
+  },
+  {
+    name: 'Nidoran / "M"',
+    height: .5,
+    weight: 9,
+    types: ['Poison'],
+  },
+  {
+    name: 'Nidorino',
+    height: .9,
+    weight: 19.5,
+    types: ['Poison'],
+  },
+  {
+    name: 'Nidoking',
+    height: 1.4,
+    weight: 62,
+    types: ['Ground', 'Posion']
+  },
+  {
+    name: 'Clefairy',
+    height: .6,
+    weight: 7.5,
+    types: ['Fairy']
+  },
+  {
+    name: 'Clefable',
+    height: 1.3,
+    weight: 40,
+    types: ['Fairy']
+  },
+  {
+    name: 'Vulpix',
+    height: .6,
+    weight: 9.9,
+    types: ['Fire']
+  },
+  {
+    name: 'Ninetails',
+    height: 1.1,
+    weight: 19.9,
+    types: ['Fire']
+  },
+  {
+    name: 'Jigglypuff',
+    height: .5,
+    weight: 5.5,
+    types: ['Fairy', 'Normal']
+  },
+  {
+    name: 'Zubat',
+    height: .8,
+    weight: 7.5,
+    types: ['Poison', 'Flying']
+  },
+  {
+    name: 'Golbat',
+    height: 1.6,
+    weight: 55,
+    types: ['Poison', 'Flying']
+  },
+  {
+    name: 'Oddish',
+    height: .5,
+    weight: 5.4,
+    types: ['Grass', 'Poison']
+  },
+  {
+    name: 'Gloom',
+    height: .8,
+    weight: 8.6,
+    types: ['Grass', 'Poison']
+  },
+  {
+    name: 'Vileplume',
+    height: 1.2,
+    weight: 18.6,
+    types: ['Grass', 'Poison']
+  },
+  {
+    name: 'Paras',
+    height: .3,
+    weight: 5.4,
+    types: ['Grass', 'Bug']
+  },
+  {
+    name: 'Parasect',
+    height: 1,
+    weight: 29.5,
+    types: ['']
+  },
+  {
+    name: 'Venonat',
+    height: 1,
+    weight: 30,
+    types: ['Bug', 'Poison']
+  },
+  {
+    name: 'Venomoth',
+    height: 1.5,
+    weight: 12.5,
+    types: ['Bug', 'Poison']
+  },
+  {
+    name: 'Diglett',
+    height: .2,
+    weight: .8,
+    types: ['Ground']
+  },
+  {
+    name: 'Dugtrio',
+    height: .7,
+    weight: 33.3,
+    types: ['Ground']
+  },
+  {
+    name: 'Meowth',
+    height: .4,
+    weight: 4.2,
+    types: ['Normal']
+  },
+  {
+    name: 'Persian',
+    height: 1,
+    weight: 32,
+    types: ['Normal']
+  },
+  {
+    name: 'Psyduck',
+    height: .8,
+    weight: 19.6,
+    types: ['Water']
+  },
+  {
+    name: 'Golduck',
+    height: ,
+    weight: ,
+    types: ['']
+  },
+  {
+    name: 'Mankey',
+    height: .5,
+    weight: 28,
+    types: ['Fighting']
+  },
+  {
+    name: 'Primeape',
+    height: 1,
+    weight: 32,
+    types: ['Fighting']
+  },
+  {
+    name: 'Growlithe',
+    height: .7,
+    weight: 19,
+    types: ['Fire']
+  },
+  {
+    name: 'Arcanine',
+    height: 1.9,
+    weight: 155,
+    types: ['Fire']
+  },
+  {
+    name: 'Poliwag',
+    height: .6,
+    weight: 12.4,
+    types: ['Water']
+  },
+  {
+    name: 'Poliwhirl',
+    height: 1,
+    weight: 20,
+    types: ['Water']
+  },
+  {
+    name: 'Poliwrath',
+    height: 1.3,
+    weight: 54,
+    types: ['Water', 'Fighting']
+  },
+  {
+    name: 'Abra',
+    height: .9,
+    weight: 19.5,
+    types: ['Pyschic']
+  },
+  {
+    name: 'Kadabra',
+    height: 1.3,
+    weight: 56.5,
+    types: ['Psychic']
+  },
+  {
+    name: 'Alakazam',
+    height: 1.5,
+    weight: 48,
+    types: ['Pyschic']
+  },
+]
